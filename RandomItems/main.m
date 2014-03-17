a02b8d0ab0131a5754f3b48ce8d2fa4688c940f6
Alexis <alexiswu@Alexiss-MacBook-Pro.local>
2014-03-17 21:30:28 +0100

    Initial Commit

---
+++ RandomItems/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Alexis on 2014-03-17.
+//  Copyright (c) 2014 Brawu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
